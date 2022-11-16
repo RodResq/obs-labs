9af89efd2135df68f6adce120c38d3590771062f
RodResq <rodrigoresque@yahoo.com.br>
2022-11-16 11:29:08 -0300

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  actvated: boolean = false;
+  private activatedSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(
+      (data: boolean) => this.actvated = data
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 }
